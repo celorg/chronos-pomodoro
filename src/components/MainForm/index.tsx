6433470b0b2bed08e74d8b40f8be10f29dca23fe
celorg <marcelinho.ribeirog.2000@gmail.com>
2025-09-17 09:58:35 -0300

    Create step cycle

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -68,9 +68,12 @@ export function MainForm () {
             <div className='formRow'>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
             </div>
-            <div className='formRow'>
-                <Cycles />
-            </div>
+            {state.currentCycle > 0 && (
+                <div className='formRow'>
+                    <Cycles />
+                </div>
+            ) }
+            
             <div className='formRow'>
                 <DefaultButton icon={<PlayCircleIcon />}   />
                 {/* <DefaultButton icon={<StopCircleIcon />} color='red'  /> */}
